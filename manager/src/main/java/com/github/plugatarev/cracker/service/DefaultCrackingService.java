6d380920fd60cfe67a037364c48c519d6ab6a9d4
plugatarev <plugatarevk@gmail.com>
2024-02-07 14:44:46 +0700

    Added logging and fix ports

--- manager/src/main/java/com/github/plugatarev/cracker/service/DefaultCrackingService.java
+++ manager/src/main/java/com/github/plugatarev/cracker/service/DefaultCrackingService.java
@@ -10,6 +10,7 @@ import dto.WorkerCrackingResponse;
 
 import lombok.RequiredArgsConstructor;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
@@ -19,6 +20,7 @@ import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class DefaultCrackingService implements CrackingService {
 
     private final CrackingRepository crackingRepository;
@@ -32,6 +34,7 @@ public class DefaultCrackingService implements CrackingService {
 
     @Override
     public RequestId submitCrackingRequest(CrackingRequest crackingRequest) {
+        log.info("Received cracking request from user, hash='{}' and maxLength='{}'", crackingRequest.hash(), crackingRequest.maxLength());
         RequestId requestId = new RequestId(UUID.randomUUID());
         TaskStatus taskStatus = new TaskStatus();
 
@@ -43,15 +46,17 @@ public class DefaultCrackingService implements CrackingService {
 
     @Override
     public TaskStatus getTaskStatus(RequestId id) throws NotFoundTaskException {
+        log.info("Received status request from user for task='{}'", id.requestId());
         TaskStatus taskStatus =
                 crackingRepository
                         .findById(id)
                         .orElseThrow(
                                 () ->
                                         new NotFoundTaskException(
-                                                "Request not found: " + id.toString()));
+                                                "Request not found: " + id));
         Duration dur = Duration.between(taskStatus.getStartTime(), Instant.now());
         if (dur.toMillis() > timeout.toMillis()) {
+            log.info("Task='{}' timeout", id);
             taskStatus.setStatus(TaskStatus.Stage.ERROR);
         }
         return taskStatus;
@@ -61,10 +66,12 @@ public class DefaultCrackingService implements CrackingService {
     public synchronized void updateTaskStatus(WorkerCrackingResponse workerResponse) {
         RequestId requestId = workerResponse.id();
         TaskStatus status = crackingRepository.findById(requestId).orElseThrow();
+        log.info("Received update request from worker for task='{}', workerPart='{}', data='{}'", requestId.requestId(), workerResponse.workerPart(), workerResponse.data());
 
         status.completeTask(workerResponse.workerPart(), workerResponse.data());
         if (status.completedTasks() == workersCount
                 && status.getStatus().equals(TaskStatus.Stage.IN_PROGRESS)) {
+            log.info("Task='{}' ready", requestId);
             status.setStatus(TaskStatus.Stage.READY);
             crackingRepository.update(requestId, status);
         }
